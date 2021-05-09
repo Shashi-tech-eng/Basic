892fa9c1a543e7e9cf005befef08e8e492f434f6
Shashi-tech-eng <shashivardhan159@gmail.com>
2021-05-09 08:00:38 +0530

    blurring

---
+++ cv/blur.py
@@ -0,0 +1,21 @@
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread("bumble.png")
+#Averaging Blur
+blur1=cv2.blur(img,(5,5))
+#Gaussian Blur 
+blur2=cv2.GaussianBlur(img,(5,5),1)
+#Median Blur
+blur3=cv2.medianBlur(img,5)
+#Bilateral Filtering
+blur4=cv2.bilateralFilter(img,3,75,75)
+
+titles=["Original","Averaging","Gaussian Blur","Median Blur","Bilateral Filter"]
+images=[img,blur1,blur2,blur3,blur4]
+count=5
+for i in range(count):
+    plt.subplot(2,3,i+1)
+    plt.title(titles[i])
+    plt.imshow(images[i])
+plt.show()
+plt.close()
