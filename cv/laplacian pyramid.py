30010140cf3fda90edc50fd1017b15b084fb7e9c
Shashi-tech-eng <shashivardhan159@gmail.com>
2021-05-14 19:30:56 +0530

    laplacian pyramid

---
+++ cv/laplacian pyramid.py
@@ -0,0 +1,17 @@
+import cv2
+img=cv2.imread("bumble.png")
+G = img.copy()
+gpA = [G]
+for i in range(6):
+    G = cv2.pyrDown(G)
+    #cv2.imshow(str(i),G)
+    gpA.append(G)
+layer=gpA[5]
+laplacian=[layer]
+for i in range(5,0,-1):
+    gaussian_extended=cv2.pyrUp(gpA[i])
+    laplacian=cv2.subtract(gpA[i-1],gaussian_extended)
+    cv2.imshow(str(i),laplacian)
+cv2.imshow("Original",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
